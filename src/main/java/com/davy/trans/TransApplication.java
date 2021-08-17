6d0d86910dde4a72d6c15133e24298b737ea026e
DavyJones27 <ajayyadavkol16@gmail.com>
2021-08-17 18:53:52 +0530

    add cors filter

--- src/main/java/com/davy/trans/TransApplication.java
+++ src/main/java/com/davy/trans/TransApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class TransApplication {
@@ -26,6 +29,21 @@ public class TransApplication {
 
         return registrationBean;
 
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        registrationBean.setFilter(new CorsFilter(source));
+        registrationBean.setOrder(0);
+
+        return registrationBean;
     }
 
 }
