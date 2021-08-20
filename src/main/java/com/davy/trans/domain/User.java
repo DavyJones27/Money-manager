ee63a728a5b12a8fb2c7b58c69fd6cbd592b9712
DavyJones27 <ajayyadavkol16@gmail.com>
2021-08-20 21:45:39 +0530

    custom validator added

--- src/main/java/com/davy/trans/domain/User.java
+++ src/main/java/com/davy/trans/domain/User.java
@@ -1,6 +1,10 @@
 package com.davy.trans.domain;
 
+import com.davy.trans.validation.EmailCheck;
 import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -10,8 +14,17 @@ import lombok.*;
 public class User {
 
     private Integer userId;
+
+    @Size(min = 2, max = 20, message = "btw 2 - 4")
     private String firstName;
+
+    @Size(min = 2, max = 4, message = "btw 2 - 4")
     private String lastName;
+
+    @Email
+    @EmailCheck
     private String email;
+
+    @Size(min = 4, max = 8, message = "btw 2 - 4")
     private String password;
 }
